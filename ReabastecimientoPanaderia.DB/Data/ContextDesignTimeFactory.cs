25592aa355a669aeaa3a1256914988d792084a4a
Mauro <cabrera.mauronaim@gmail.com>
2026-04-09 20:31:15 -0300

    Eliminar comentarios de ContextDesignTimeFactory

--- ReabastecimientoPanaderia.DB/Data/ContextDesignTimeFactory.cs
+++ ReabastecimientoPanaderia.DB/Data/ContextDesignTimeFactory.cs
@@ -14,12 +14,9 @@ namespace ReabastecimientoPanaderia.DB.Data
             // que usarías en tu aplicación real (pero puede ser una específica para diseño).
             var optionsBuilder = new DbContextOptionsBuilder<Context>();
 
-            // EJEMPLO CON SQL SERVER:
             // Reemplaza "Server=...;Database=...;..." con tu cadena real.
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ReabastecimientoPanaderia;Trusted_Connection=True;MultipleActiveResultSets=true");
 
-            // EJEMPLO CON SQLite:
-            // optionsBuilder.UseSqlite("Data Source=MiBaseDeDatos.db");
 
             return new Context(optionsBuilder.Options);
         }
