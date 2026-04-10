00abf30b35a22177ca1a21e167a3603630466fcc
Mauro <cabrera.mauronaim@gmail.com>
2026-04-09 23:35:20 -0300

    Mejorar configuracion de OnModelBuilder

--- ReabastecimientoPanaderia.DB/Data/Context.cs
+++ ReabastecimientoPanaderia.DB/Data/Context.cs
@@ -22,21 +22,69 @@ namespace ReabastecimientoPanaderia.DB.Data
         {
             base.OnModelCreating(modelBuilder);
             // Configuración de relaciones y restricciones
-            modelBuilder.Entity<Producto>()
-                .HasOne(p => p.TipoProducto)
+            modelBuilder.Entity<TipoProducto>(entity =>
+            {
+                entity.HasMany(tp => tp.Productos)
+                    .WithOne(p => p.TipoProducto)
+                    .HasForeignKey(p => p.TipoProductoID)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.Property(tp => tp.Codigo)
+                                .HasMaxLength(3)
+                                .IsRequired();
+                entity.Property(tp => tp.Nombre)
+                                .HasMaxLength(50)
+                                .IsRequired();
+
+                entity.HasIndex(tp => tp.Codigo).IsUnique();
+                entity.HasIndex(tp => tp.Nombre).IsUnique();
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasOne(p => p.TipoProducto)
                 .WithMany(tp => tp.Productos)
                 .HasForeignKey(p => p.TipoProductoID)
                 .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<Renglon>()
-                .HasOne(r => r.ProductoSolicitado)
+
+                entity.Property(p => p.Nombre)
+                                .HasMaxLength(100)
+                                .IsRequired();
+
+                entity.Property(p => p.TipoProductoID).IsRequired();
+
+                entity.HasIndex(p => p.Nombre).IsUnique();
+                entity.HasIndex(p => new { p.TipoProductoID, p.Nombre }).IsUnique();
+
+
+            });
+
+            modelBuilder.Entity<Pedido>(entity =>
+            {
+                entity.Property(p => p.FechaYHora)
+                                .IsRequired();
+
+                entity.HasIndex(p => p.FechaYHora);
+            });
+
+            modelBuilder.Entity<Renglon>(entity =>
+            {
+                entity.HasOne(r => r.Pedido)
+                    .WithMany(p => p.Renglones)
+                    .HasForeignKey(r => r.PedidoID)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(r => r.ProductoSolicitado)
                 .WithMany()
                 .HasForeignKey(r => r.ProductoSolicitadoID)
                 .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<Renglon>()
-                .HasOne(r => r.Pedido)
-                .WithMany(p => p.Renglones)
-                .HasForeignKey(r => r.PedidoID)
-                .OnDelete(DeleteBehavior.Cascade);
+
+                entity.Property(r => r.CantidadSolicitada)
+                                .IsRequired();
+
+                entity.HasIndex(r => new { r.PedidoID, r.ProductoSolicitadoID })
+                                .IsUnique();
+            });
         }
     }
 }
