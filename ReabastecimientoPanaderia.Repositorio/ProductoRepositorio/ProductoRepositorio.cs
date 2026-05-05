2b6cb9ba5d944c42f48fa3fd14e2d8c880b3b91c
Mauro <cabrera.mauronaim@gmail.com>
2026-05-05 20:02:35 -0300

    Agregar ProductoRepositorio

---
+++ ReabastecimientoPanaderia.Repositorio/ProductoRepositorio/ProductoRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReabastecimientoPanaderia.DB.Data;
+using ReabastecimientoPanaderia.DB.Data.Entities;
+using ReabastecimientoPanaderia.Repositorio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReabastecimientoPanaderia.Repositorio.ProductoRepositorio
+{
+    public class ProductoRepositorio : Repositorio<Producto>
+    {
+        private readonly Context _context;
+
+        public ProductoRepositorio(Context context) : base(context)
+        {
+            this._context = context;
+        }
+        public async Task<List<Producto>> FullGetAll()
+        {
+            return await _context.Productos
+                .Include(p => p.TipoProducto)
+                .Include(p => p.EsComun)
+                .ToListAsync();
+        }
+        public async Task<Producto?> FullGetById(int id)
+        {
+            return await _context.Productos
+                .Include(p => p.TipoProducto)
+                .Include(p => p.EsComun)
+                .FirstOrDefaultAsync(p => p.ID == id);
+        }
+    }
+}
